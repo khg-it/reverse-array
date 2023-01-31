5cc250bd3b81994c9f88001fa83e218a98d7a320
khg-it <120780238+khg-it@users.noreply.github.com>
2023-01-31 01:20:21 -0800

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,10 @@
+public class reverse {
+    public static void main(String args[])
+    {
+        int ar[]={1,2,3,4};
+        for(int i=3;i>=0;i--)
+        {
+            System.out.print(ar[i]+" ");
+        }
+    }
+}
